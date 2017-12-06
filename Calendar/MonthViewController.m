e9b20ab226a44d457fae89d16eb93c68c2035923
AhsanMahmood <ahsanmahmood@AhsanMahmoods-Mac.local>
2017-12-06 12:41:33 -0800

    9. Implemented Day of week integer Added method to change labels for month and year in viewDidLoad implemented numOfDaysInMonth method implemented a method to determined the first day of the week to know what day the dates start from

--- Calendar/MonthViewController.m
+++ Calendar/MonthViewController.m
@@ -16,6 +16,7 @@ int theYear;
 int theMonth;
 int firstDayOfMonth;
 int numOfDaysInMonth;
+int dayOfWeek;
 
 
 
@@ -24,9 +25,10 @@ int numOfDaysInMonth;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-
-    
-    
+    _yearLabel.text =[NSString stringWithFormat:@"%d", theYear];
+    _monthLabel.text =[NSString stringWithFormat:@"%d", theMonth];
+    NSLog(@"dayOfWeek = %d", dayOfWeek);
+    NSLog(@"NumberOfDaysInMonth = %d", numOfDaysInMonth);
     
 }
 
@@ -43,6 +45,32 @@ int numOfDaysInMonth;
         theYear--;
     }
     
+}
+
+-(NSUInteger)daysInMonth:(NSDate *)myDate{
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    NSRange rng = [cal rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:myDate];
+    NSUInteger numOfDaysInMonth = rng.length;
+    NSLog(@"NumberOfDaysInMonth%d", numOfDaysInMonth);
+    return numOfDaysInMonth;
+}
+
+
+-(void)dateInfo{
+    
+    
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setDay:1];
+    [components setMonth:theMonth];
+    [components setYear:theYear];
+    NSDate * newDate = [calendar dateFromComponents:components];
+    NSDateComponents *comps = [gregorian components:NSCalendarUnitWeekday fromDate:newDate];
+    dayOfWeek=[comps weekday];
+    NSLog(@"NumberOfDaysInMonth%d", dayOfWeek);
+    //determines the the first day of the month based of theMonth and theYear from the cell selected.
+
 }
 
 - (IBAction)lastMonth:(id)sender {
